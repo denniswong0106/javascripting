1192dc3b17ec0a6cbe449df5d1c70b0a3d795f3f
Dennis Wong <denniswong0106@gmail.com>
2021-02-03 23:40:05 +0000

    initial commit with all my work so far

---
+++ array-filtering.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let filtered = numbers.filter(function (num) {
+  return (num % 2) === 0; // to check if num is even or not
+  });
+
+  console.log(filtered);
+
---
+++ introduction.js
@@ -0,0 +1,1 @@
+console.log('hello'); //test see if works
